8e3244d815a5da5c407214e7974f3b795ad657f0
Maniz Shrestha <mashres15@gmail.com>
2017-06-11 15:21:07 -0400

    Final version

--- app.js
+++ app.js
@@ -12,7 +12,10 @@ const app = {
 
         this.load()
     },
-
+    
+    /*-----------------------------------------
+    Method to load the flick from localStorage
+    ------------------------------------------*/
     load() {
         // Get the JSON string out of localStorage
         const flicksJSON = localStorage.getItem('flicks')
@@ -43,14 +46,15 @@ const app = {
     addFlickViaForm(ev) {
         ev.preventDefault()
         const f = ev.target
+        //console.log(f.flickName.value)
         const flick = {
             id: this.max + 1,
             name: f.flickName.value,
             year: f.year.value,
+            promote: false,
         }
 
         this.addFlick(flick)
-
         f.reset()
     },
 
@@ -64,10 +68,15 @@ const app = {
         const item = this.template.cloneNode(true)
         item.classList.remove('template')
         item.dataset.id = flick.id
+        
+        if(flick.promote){
+            item.classList.add('promote')
+        }
+        
         item
             .querySelector('.flick-name')
             .textContent = flick.name
-        
+
         item
             .querySelector('.flick-year')
             .textContent = flick.year
@@ -77,13 +86,17 @@ const app = {
             .addEventListener('click', this.removeFlick.bind(this))
 
         item
+            .querySelector('button.edit')
+            .addEventListener('click', this.editText.bind(this))
+
+        item
             .querySelector('button.fav')
-            .addEventListener('click', this.promoteFlick.bind(this))
-        
+            .addEventListener('click', this.promoteFlick.bind(this, flick))
+
         item
             .querySelector('button.up')
             .addEventListener('click', this.moveUp.bind(this, flick))
-        
+
         item
             .querySelector('button.down')
             .addEventListener('click', this.moveDown.bind(this, flick))
@@ -91,11 +104,47 @@ const app = {
         return item
     },
 
-    promoteFlick(ev){
+    editText(ev){
+        const editbutton = ev.target
         const listItem = ev.target.closest('.flick')
+        const flickName = listItem.firstElementChild
+        if(flickName.isContentEditable){
+            //console.log("in true")
+            flickName.contentEditable = false
+            editbutton.classList.add('fa-pencil-square-o')
+            editbutton.classList.remove('fa-check')
+            editbutton.classList.add('warning')
+            editbutton.classList.remove('success')
+
+            // Find the flick in the array, and remove it
+            for (let i = 0; i < this.flicks.length; i++) {
+                const currentId = this.flicks[i].id.toString()
+                if (listItem.dataset.id === currentId) {
+                    this.flicks[i].name = flickName.innerHTML
+                    break
+                }
+            }
+            //console.log(this.flicks)
+            this.save()
+        }
+        else{
+            //console.log("false")
+            flickName.contentEditable = true
+            flickName.focus()
+            editbutton.classList.add('fa-check')
+            editbutton.classList.remove('fa-pencil-square-o')
+            editbutton.classList.remove('warning')
+            editbutton.classList.add('success')
+        }
+    },
+
+    promoteFlick(flick, ev){
+        const listItem = ev.target.closest('.flick')
+        flick.promote = !flick.promote
 
         // toggle promote
         listItem.classList.toggle('promote')
+        this.save()
     },
 
     removeFlick(ev) {
@@ -117,14 +166,16 @@ const app = {
     moveUp(flick, ev) {
         const li = ev.target.closest('.flick')
 
-        const index = this.flicks.findIndex((currentFlick, i) => {
+        const index = this.flicks.findIndex((currentFlick) => {
             return currentFlick.id === flick.id
         })
+        console.log(this.flicks)
+        console.log(index)
 
         if (index > 0) {
             // Changing the DOM
             this.list.insertBefore(li, li.previousElementSibling)
-            
+
             //Swap the flicks in the array
             const previousFlick = this.flicks[index - 1]
             this.flicks[index - 1] = flick
@@ -137,10 +188,10 @@ const app = {
     moveDown(flick, ev) {
         const li = ev.target.closest('.flick')
 
-        const index = this.flicks.findIndex((currentFlick, i) => {
+        const index = this.flicks.findIndex((currentFlick) => {
             return currentFlick.id === flick.id
         })
-
+        
         if (index < this.flicks.length - 1) {
             this.list.insertBefore(li.nextSibling, li)
 
